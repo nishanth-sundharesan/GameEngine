a94765ebfa4434b12351fdff1596943feaad650b
Nishanth Sundharesan <nishanth.reddy992@gmail.com>
2017-01-21 13:12:18 -0500

    Moved the user implementation of the templated ToString method for the user defined type to a separate(newly created) header file.

---
+++ Source/Library.Desktop.Test/ToStringTemplates.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Foo.h"
+#include "CppUnitTest.h"
+
+namespace Microsoft
+{
+	namespace VisualStudio
+	{
+		namespace CppUnitTestFramework
+		{
+			/*template<>
+			inline std::wstring ToString<UnitTestSupportClasses::Foo>(const UnitTestSupportClasses::Foo& t)
+			{
+				RETURN_WIDE_STRING(t.GetIntegerData());
+			}*/
+		}
+	}
+}

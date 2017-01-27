15fb20ee9391858d2513cf5f3e704252ae04e192
Nishanth Sundharesan <nishanth.reddy992@gmail.com>
2017-01-26 22:53:05 -0500

    Initial commit of the Vector Unit test file.

---
+++ Source/Library.Desktop.Test/VectorTest.cpp
@@ -0,0 +1,50 @@
+#include "Pch.h"
+#include "CppUnitTest.h"
+#include "Vector.h"
+#include "Foo.h"
+#include "ToStringTemplates.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace GameEngineLibrary;
+using namespace UnitTestSupportClasses;
+using namespace std;
+
+namespace LibraryDesktopTest
+{
+	TEST_CLASS(VectorTest)
+	{
+	public:
+		TEST_METHOD_INITIALIZE(Initialize)
+		{
+			_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF);
+			_CrtMemCheckpoint(&sStartMemState);
+		}
+
+		TEST_METHOD_CLEANUP(Cleanup)
+		{
+#pragma warning(suppress: 4101)															//Suppressing the warning "'endMemState': unreferenced local variable" and "'diffMemState': unreferenced local variable" in release mode
+			_CrtMemState endMemState, diffMemState;
+			_CrtMemCheckpoint(&endMemState);
+			if (_CrtMemDifference(&diffMemState, &sStartMemState, &endMemState))
+			{
+				_CrtMemDumpStatistics(&diffMemState);
+				Assert::Fail(L"Memory Leaks!");
+			}
+		}
+
+
+		template<typename T>
+		void TestDefaultConstructor(Vector<T>& vector)
+		{
+
+		}
+
+		TEST_METHOD(TestMethod1)
+		{
+			
+		}
+	private:
+		static _CrtMemState sStartMemState;
+	};
+	_CrtMemState VectorTest::sStartMemState;
+}

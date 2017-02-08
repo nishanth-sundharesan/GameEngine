bb2c65d7e994f425ffc9dfb0bd433d3e0506b071
Nishanth Sundharesan <nishanth.reddy992@gmail.com>
2017-02-08 17:15:04 -0500

    Removed the SListIteratorTest.cpp file and added all the code to SListTest.cpp

--- Source/Library.Desktop.Test/SListTest.cpp
+++ Source/Library.Desktop.Test/SListTest.cpp
@@ -1183,6 +1183,344 @@ namespace LibraryDesktopTest
 		/*----------------------------------------------------------------------*/
 		/* END																	*/
 		/*----------------------------------------------------------------------*/
+
+		/************************************************************************/
+		/* BEGIN		Unit testing for the "Overloaded == and != Operator".	*/
+		/************************************************************************/
+		TEST_METHOD(SListIteratorEqualAndNotEqualOperator)
+		{
+			SList<int32_t>::Iterator iteratorOne;
+			SList<int32_t>::Iterator iteratorTwo;
+
+			Assert::IsTrue(iteratorOne == iteratorTwo);
+			Assert::IsFalse(iteratorOne != iteratorTwo);
+
+			int32_t firstPushedVariable = 10;
+			SList<int32_t> listOne;
+			listOne.PushBack(firstPushedVariable);
+
+			iteratorOne = listOne.begin();
+			iteratorTwo = listOne.begin();
+
+			Assert::IsTrue(iteratorOne == iteratorTwo);
+			Assert::IsFalse(iteratorOne != iteratorTwo);
+
+			++iteratorOne;
+			Assert::IsFalse(iteratorOne == iteratorTwo);
+			Assert::IsTrue(iteratorOne != iteratorTwo);
+
+			++iteratorTwo;
+			Assert::IsTrue(iteratorOne == iteratorTwo);
+			Assert::IsFalse(iteratorOne != iteratorTwo);
+		}
+		/*----------------------------------------------------------------------*/
+		/* END																	*/
+		/*----------------------------------------------------------------------*/
+
+
+		/************************************************************************/
+		/* BEGIN		Unit testing for the "Overloaded ++ Operator".			*/
+		/************************************************************************/
+		TEST_METHOD(SListIteratorIntIncrementOperator)
+		{
+			SList<int32_t>::Iterator iterator;
+
+			//Checking the empty Iterator with Prefix ++ operator
+			auto functionPrefixIncrementEmpty = [&iterator] { ++iterator; };
+			Assert::ExpectException<exception>(functionPrefixIncrementEmpty);
+
+			//Checking the empty Iterator with Postfix ++ operator
+			auto functionPostfixIncrementEmpty = [&iterator] { iterator++; };
+			Assert::ExpectException<exception>(functionPostfixIncrementEmpty);
+
+			SList<int32_t> list;
+			iterator = list.end();
+
+			//Checking the Iterator pointing to the end of the list with Prefix ++ operator
+			auto functionPrefixIncrementEnd = [&iterator] { ++iterator; };
+			Assert::ExpectException<exception>(functionPrefixIncrementEnd);
+
+			//Checking the Iterator pointing to the end of the list with Postfix ++ operator
+			auto functionPostfixIncrementEnd = [&iterator] { iterator++; };
+			Assert::ExpectException<exception>(functionPostfixIncrementEnd);
+
+
+			//Checking if the pushed value matches the value from the Iterator after using the increment operator on the Iterator
+			int32_t firstPushedVariable = 10;
+			int32_t secondPushedVariable = 20;
+			list.PushBack(firstPushedVariable);
+			list.PushBack(secondPushedVariable);
+
+			iterator = list.begin();
+			++iterator;
+			Assert::AreEqual(*iterator, secondPushedVariable);
+
+			iterator = list.begin();
+			iterator++;
+			Assert::AreEqual(*iterator, secondPushedVariable);
+
+			//Incrementing the Iterator to the end and checking if it matches the end() iterator
+			++iterator;
+			Assert::IsTrue(iterator == list.end());
+			Assert::ExpectException<exception>([&iterator] { ++iterator; });
+
+			iterator = list.begin();
+			iterator++;
+			iterator++;
+			Assert::IsTrue(iterator == list.end());
+			Assert::ExpectException<exception>([&iterator] { iterator++; });
+		}
+
+		TEST_METHOD(SListIteratorIntPointerIncrementOperator)
+		{
+			SList<int32_t*>::Iterator iterator;
+
+			//Checking the empty Iterator with Prefix ++ operator
+			auto functionPrefixIncrementEmpty = [&iterator] { ++iterator; };
+			Assert::ExpectException<exception>(functionPrefixIncrementEmpty);
+
+			//Checking the empty Iterator with Postfix ++ operator
+			auto functionPostfixIncrementEmpty = [&iterator] { iterator++; };
+			Assert::ExpectException<exception>(functionPostfixIncrementEmpty);
+
+			SList<int32_t*> list;
+			iterator = list.end();
+
+			//Checking the Iterator pointing to the end of the list with Prefix ++ operator
+			auto functionPrefixIncrementEnd = [&iterator] { ++iterator; };
+			Assert::ExpectException<exception>(functionPrefixIncrementEnd);
+
+			//Checking the Iterator pointing to the end of the list with Postfix ++ operator
+			auto functionPostfixIncrementEnd = [&iterator] { iterator++; };
+			Assert::ExpectException<exception>(functionPostfixIncrementEnd);
+
+
+			//Checking if the pushed value matches the value from the Iterator after using the increment operator on the Iterator
+			int32_t firstPushedVariable = 10;
+			int32_t secondPushedVariable = 20;
+			list.PushBack(&firstPushedVariable);
+			list.PushBack(&secondPushedVariable);
+
+			iterator = list.begin();
+			++iterator;
+			Assert::AreEqual(*(*iterator), secondPushedVariable);
+
+			iterator = list.begin();
+			iterator++;
+			Assert::AreEqual(*(*iterator), secondPushedVariable);
+
+			//Incrementing the Iterator to the end and checking if it matches the end() iterator
+			++iterator;
+			Assert::IsTrue(iterator == list.end());
+			Assert::ExpectException<exception>([&iterator] { ++iterator; });
+
+			iterator = list.begin();
+			iterator++;
+			iterator++;
+			Assert::IsTrue(iterator == list.end());
+			Assert::ExpectException<exception>([&iterator] { iterator++; });
+		}
+
+		TEST_METHOD(SListIteratorUserDefinedTypeIncrementOperator)
+		{
+			SList<Foo>::Iterator iterator;
+
+			//Checking the empty Iterator with Prefix ++ operator
+			auto functionPrefixIncrementEmpty = [&iterator] { ++iterator; };
+			Assert::ExpectException<exception>(functionPrefixIncrementEmpty);
+
+			//Checking the empty Iterator with Postfix ++ operator
+			auto functionPostfixIncrementEmpty = [&iterator] { iterator++; };
+			Assert::ExpectException<exception>(functionPostfixIncrementEmpty);
+
+			SList<Foo> list;
+			iterator = list.end();
+
+			//Checking the Iterator pointing to the end of the list with Prefix ++ operator
+			auto functionPrefixIncrementEnd = [&iterator] { ++iterator; };
+			Assert::ExpectException<exception>(functionPrefixIncrementEnd);
+
+			//Checking the Iterator pointing to the end of the list with Postfix ++ operator
+			auto functionPostfixIncrementEnd = [&iterator] { iterator++; };
+			Assert::ExpectException<exception>(functionPostfixIncrementEnd);
+
+
+			//Checking if the pushed value matches the value from the Iterator after using the increment operator on the Iterator
+			Foo firstPushedVariable(10);
+			Foo secondPushedVariable(20);
+			list.PushBack(firstPushedVariable);
+			list.PushBack(secondPushedVariable);
+
+			iterator = list.begin();
+			++iterator;
+			Assert::AreEqual(*iterator, secondPushedVariable);
+
+			iterator = list.begin();
+			iterator++;
+			Assert::AreEqual(*iterator, secondPushedVariable);
+
+			//Incrementing the Iterator to the end and checking if it matches the end() iterator
+			++iterator;
+			Assert::IsTrue(iterator == list.end());
+			Assert::ExpectException<exception>([&iterator] { ++iterator; });
+
+			iterator = list.begin();
+			iterator++;
+			iterator++;
+			Assert::IsTrue(iterator == list.end());
+			Assert::ExpectException<exception>([&iterator] { iterator++; });
+		}
+		/*----------------------------------------------------------------------*/
+		/* END																	*/
+		/*----------------------------------------------------------------------*/
+
+
+		/************************************************************************/
+		/* BEGIN		Unit testing for the "Overloaded * Operator".			*/
+		/************************************************************************/
+		TEST_METHOD(SListIteratorIntDereferenceOperator)
+		{
+			//Testing the non const (*)Dereference operator.
+			SList<int32_t>::Iterator iterator;
+			Assert::ExpectException<exception>([&iterator] { *iterator; });
+
+			SList<int32_t> list;
+			iterator = list.begin();
+			Assert::ExpectException<exception>([&iterator] { *iterator; });
+
+			iterator = list.end();
+			Assert::ExpectException<exception>([&iterator] { *iterator; });
+
+			int32_t firstPushedVariable = 10;
+			int32_t secondPushedVariable = 20;
+			list.PushBack(firstPushedVariable);
+			list.PushBack(secondPushedVariable);
+
+			iterator = list.begin();
+			Assert::AreEqual(*iterator, firstPushedVariable);
+			++iterator;
+			Assert::AreEqual(*iterator, secondPushedVariable);
+			list.Clear();
+
+
+			//Testing the const (*)Dereference operator.
+			const SList<int32_t>::Iterator iteratorConstEmpty;
+			Assert::ExpectException<exception>([&iteratorConstEmpty] { *iteratorConstEmpty; });
+
+			const SList<int32_t>::Iterator iteratorConstBegin = list.begin();
+			Assert::ExpectException<exception>([&iteratorConstBegin] { *iteratorConstBegin; });
+
+			const SList<int32_t>::Iterator iteratorConstEnd = list.end();
+			Assert::ExpectException<exception>([&iteratorConstEnd] { *iteratorConstEnd; });
+
+			list.PushBack(firstPushedVariable);
+			list.PushBack(secondPushedVariable);
+
+			const SList<int32_t>::Iterator iteratorConstFirstItem = list.begin();
+			Assert::AreEqual(*iteratorConstFirstItem, firstPushedVariable);
+
+			iterator = list.begin();
+			iterator++;
+			const SList<int32_t>::Iterator iteratorConstSecondItem = iterator;
+			Assert::AreEqual(*iteratorConstSecondItem, secondPushedVariable);
+		}
+
+		TEST_METHOD(SListIteratorIntPointerDereferenceOperator)
+		{
+			//Testing the non const (*)Dereference operator.
+			SList<int32_t*>::Iterator iterator;
+			Assert::ExpectException<exception>([&iterator] { *iterator; });
+
+			SList<int32_t*> list;
+			iterator = list.begin();
+			Assert::ExpectException<exception>([&iterator] { *iterator; });
+
+			iterator = list.end();
+			Assert::ExpectException<exception>([&iterator] { *iterator; });
+
+			int32_t firstPushedVariable = 10;
+			int32_t secondPushedVariable = 20;
+			list.PushBack(&firstPushedVariable);
+			list.PushBack(&secondPushedVariable);
+
+			iterator = list.begin();
+			Assert::AreEqual(*(*iterator), firstPushedVariable);
+			++iterator;
+			Assert::AreEqual(*(*iterator), secondPushedVariable);
+			list.Clear();
+
+
+			//Testing the const (*)Dereference operator.
+			const SList<int32_t*>::Iterator iteratorConstEmpty;
+			Assert::ExpectException<exception>([&iteratorConstEmpty] { *iteratorConstEmpty; });
+
+			const SList<int32_t*>::Iterator iteratorConstBegin = list.begin();
+			Assert::ExpectException<exception>([&iteratorConstBegin] { *iteratorConstBegin; });
+
+			const SList<int32_t*>::Iterator iteratorConstEnd = list.end();
+			Assert::ExpectException<exception>([&iteratorConstEnd] { *iteratorConstEnd; });
+
+			list.PushBack(&firstPushedVariable);
+			list.PushBack(&secondPushedVariable);
+
+			const SList<int32_t*>::Iterator iteratorConstFirstItem = list.begin();
+			Assert::AreEqual(*(*iteratorConstFirstItem), firstPushedVariable);
+
+			iterator = list.begin();
+			iterator++;
+			const SList<int32_t*>::Iterator iteratorConstSecondItem = iterator;
+			Assert::AreEqual(*(*iteratorConstSecondItem), secondPushedVariable);
+		}
+
+		TEST_METHOD(SListIteratorUserDefinedTypeDereferenceOperator)
+		{
+			//Testing the non const (*)Dereference operator.
+			SList<Foo>::Iterator iterator;
+			Assert::ExpectException<exception>([&iterator] { *iterator; });
+
+			SList<Foo> list;
+			iterator = list.begin();
+			Assert::ExpectException<exception>([&iterator] { *iterator; });
+
+			iterator = list.end();
+			Assert::ExpectException<exception>([&iterator] { *iterator; });
+
+			Foo firstPushedVariable(10);
+			Foo secondPushedVariable(20);
+			list.PushBack(firstPushedVariable);
+			list.PushBack(secondPushedVariable);
+
+			iterator = list.begin();
+			Assert::AreEqual(*iterator, firstPushedVariable);
+			++iterator;
+			Assert::AreEqual(*iterator, secondPushedVariable);
+			list.Clear();
+
+
+			//Testing the const (*)Dereference operator.
+			const SList<Foo>::Iterator iteratorConstEmpty;
+			Assert::ExpectException<exception>([&iteratorConstEmpty] { *iteratorConstEmpty; });
+
+			const SList<Foo>::Iterator iteratorConstBegin = list.begin();
+			Assert::ExpectException<exception>([&iteratorConstBegin] { *iteratorConstBegin; });
+
+			const SList<Foo>::Iterator iteratorConstEnd = list.end();
+			Assert::ExpectException<exception>([&iteratorConstEnd] { *iteratorConstEnd; });
+
+			list.PushBack(firstPushedVariable);
+			list.PushBack(secondPushedVariable);
+
+			const SList<Foo>::Iterator iteratorConstFirstItem = list.begin();
+			Assert::AreEqual(*iteratorConstFirstItem, firstPushedVariable);
+
+			iterator = list.begin();
+			iterator++;
+			const SList<Foo>::Iterator iteratorConstSecondItem = iterator;
+			Assert::AreEqual(*iteratorConstSecondItem, secondPushedVariable);
+		}
+		/*----------------------------------------------------------------------*/
+		/* END																	*/
+		/*----------------------------------------------------------------------*/
 	private:
 		static _CrtMemState sStartMemState;
 	};

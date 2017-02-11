c2c357c0ef8874b69c3d456810062aeacae238e7
Nishanth Sundharesan <nishanth.reddy992@gmail.com>
2017-02-11 14:58:49 -0500

    Initial commit of the Unit test file for Datum.

---
+++ Source/Library.Desktop.Test/DatumTest.cpp
@@ -0,0 +1,18 @@
+#include "Pch.h"
+#include "CppUnitTest.h"
+#include "Datum.h"
+
+using namespace GameEngineLibrary;
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace LibraryDesktopTest
+{		
+	TEST_CLASS(LibraryDesktopTest)
+	{
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+			Datum d;
+		}
+	};
+}

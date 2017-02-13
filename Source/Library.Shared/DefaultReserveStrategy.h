ff65d3d03aa1d07a495bfc79551ad3b97e37c400
Nishanth Sundharesan <nishanth.reddy992@gmail.com>
2017-02-12 22:30:26 -0500

    Moved the DefaultReserveStrategyFunctor to this header file.

--- Source/Library.Shared/DefaultReserveStrategy.h
+++ Source/Library.Shared/DefaultReserveStrategy.h
@@ -1,2 +1,15 @@
 #pragma once
 #include <cstdint>
+
+namespace GameEngineLibrary
+{
+	class DefaultReserveStrategy
+	{
+	public:
+		std::uint32_t DefaultReserveStrategy::operator()(std::uint32_t size, uint32_t capacity) const
+		{
+			size;
+			return capacity * 2;
+		}
+	};
+}

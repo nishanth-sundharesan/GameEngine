ff9204c7e03a7d50ad9c4185a47c8aec4801ce07
Nishanth Sundharesan <nishanth.reddy992@gmail.com>
2017-02-16 12:19:18 -0500

    Added basic function declarations.

--- Source/Library.Shared/Scope.cpp
+++ Source/Library.Shared/Scope.cpp
@@ -1,2 +1,140 @@
 #include "Pch.h"
 #include "Scope.h"
+
+using namespace std;
+
+namespace GameEngineLibrary
+{
+	Scope::Scope(const uint32_t size) :
+		vectorArray(1U), hashmap(1U), parentScope(nullptr)
+	{
+		size;
+	}
+
+	Scope::Scope(const Scope& rhs)
+	{
+		rhs;
+	}
+
+	Scope& Scope::operator=(const Scope& rhs)
+	{
+		rhs;
+		return *this;
+	}
+
+	Scope::~Scope()
+	{
+
+	}
+	Datum* Scope::Find(const std::string& name)
+	{
+		return const_cast<Datum*>(const_cast<const Scope*>(this)->Find(name));
+	}
+
+	const Datum* Scope::Find(const std::string& name) const
+	{
+		return &tempDatum;
+		name;
+	}
+
+	Datum* Scope::Search(const std::string& name, Scope** scope)
+	{
+		return const_cast<Datum*>(const_cast<const Scope*>(this)->Search(name, scope));
+	}
+
+	const Datum* Scope::Search(const std::string& name, Scope** scope) const
+	{
+		return &tempDatum;
+		name;
+		scope;
+	}
+
+	Datum& Scope::Append(const std::string& name)
+	{
+		bool isNewValueInserted = false;
+		PairType pair = make_pair(name, Datum());
+		hashmapIterator = hashmap.Insert(pair, isNewValueInserted);
+
+		if (isNewValueInserted)
+		{
+			vectorArray.PushBack(&pair);
+		}
+		return (hashmapIterator->second);
+	}
+
+	Scope& Scope::AppendScope(const std::string& name)
+	{
+		return *this;
+		name;
+	}
+	void Scope::Adopt(Scope& childScope, const std::string& name, const std::uint32_t index)
+	{
+		//Adopt calls oprhan();
+		childScope;
+		name;
+		index;
+	}
+
+	Scope* Scope::GetParent()
+	{
+		return nullptr;
+	}
+
+	const Scope* Scope::GetParent() const
+	{
+		return nullptr;
+	}
+
+	Datum& Scope::operator[](const std::string& name)
+	{
+		return tempDatum;
+		name;
+	}
+
+	const Datum& Scope::operator[](const std::string& name) const
+	{
+		//This function throws exception if it doesn't find the Datum
+		return tempDatum;
+		name;
+	}
+
+	Datum& Scope::operator[](const std::uint32_t index)
+	{
+		return tempDatum;
+		index;
+	}
+
+	const Datum& Scope::operator[](const std::uint32_t index) const
+	{
+		return tempDatum;
+		index;
+	}
+
+	bool Scope::operator==(const Scope& rhsScope) const
+	{
+		return false;
+		rhsScope;
+	}
+
+	bool Scope::operator!=(const Scope & rhsScope) const
+	{
+		return false;
+		rhsScope;
+	}
+
+	std::string Scope::FindName(const Scope* scope) const
+	{
+		return std::string();
+		scope;
+	}
+
+	void Scope::Clear()
+	{
+
+	}
+
+	void Scope::Orphan()
+	{
+		//detach the scope from it's parent scope.
+	}
+}
--- Source/Library.Shared/Scope.h
+++ Source/Library.Shared/Scope.h
@@ -1,1 +1,69 @@
 #pragma once
+#include "Vector.h"
+#include "Hashmap.h"
+#include "Datum.h"
+
+namespace GameEngineLibrary
+{
+	class Scope
+	{
+		typedef std::pair<std::string, Datum> PairType;
+
+	public:
+		Scope(const std::uint32_t size = 1);
+
+		Scope(const Scope& rhs);
+
+		Scope& operator=(const Scope& rhs);
+
+		~Scope();
+
+		Datum* Find(const std::string& name);
+
+		const Datum* Find(const std::string& name) const;
+
+		Datum* Search(const std::string& name, Scope** scope = nullptr);
+
+		const Datum* Search(const std::string& name, Scope** scope = nullptr) const;
+
+		Datum& Append(const std::string& name);
+
+		Scope& AppendScope(const std::string& name);
+
+		void Adopt(Scope& childScope, const std::string& name, const std::uint32_t index);
+
+		Scope* GetParent(); 
+
+		const Scope* GetParent() const;
+
+		Datum& operator[](const std::string& name);
+
+		const Datum& operator[](const std::string& name) const;
+
+		Datum& operator[](const std::uint32_t index);
+
+		const Datum& operator[](const std::uint32_t index) const;
+
+		bool operator==(const Scope& rhsScope) const;
+
+		bool operator!=(const Scope& rhsScope) const;
+
+		std::string FindName(const Scope* scope) const;
+
+		void Clear();
+
+	private:
+		Vector<PairType*> vectorArray;
+
+		Hashmap<std::string, Datum> hashmap;
+
+		Hashmap<std::string, Datum>::Iterator hashmapIterator;
+
+		Scope* parentScope;
+
+		void Orphan();
+
+		//TODO - remove later
+		Datum tempDatum;
+	};
+}

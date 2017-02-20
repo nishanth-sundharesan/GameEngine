c3e72949ffa9e99f0337c482184ba56294fa7eaa
Nishanth Sundharesan <nishanth.reddy992@gmail.com>
2017-02-19 22:19:14 -0500

    Performed more unit tests.

--- Source/Library.Desktop.Test/ScopeTest.cpp
+++ Source/Library.Desktop.Test/ScopeTest.cpp
@@ -226,6 +226,8 @@ namespace LibraryDesktopTest
 			Assert::ExpectException<exception>([&] {scope.AppendScope(nameTwo); });
 
 			Scope& secondAppendedScope = scope.AppendScope(nameThree);
+			Scope emptyScope = scope.AppendScope(nameThree);
+
 			secondAppendedScope.Clear();
 		}
 
@@ -416,6 +418,7 @@ namespace LibraryDesktopTest
 			//Testing overridden .Equals()
 			Datum testDatum = *scope.Find(nameOne);
 			Assert::IsTrue(testDatum == firstAppend);
+
 		}
 
 		TEST_METHOD(ScopeTestOverloadedInEqualityOperator)
@@ -502,9 +505,192 @@ namespace LibraryDesktopTest
 			Assert::IsTrue(scope.FindName(testScope) == string());
 
 			thirdAppend.PushBack(testScope);
-			Assert::IsTrue(scope.FindName(testScope) == nameThree);			
+			Assert::IsTrue(scope.FindName(testScope) == nameThree);
 		}
 
+		TEST_METHOD(ScopeTestToString)
+		{
+			Scope scope;
+			string nameOne = "Hello";
+			string nameTwo = "World";
+			string nameThree = "Now";
 
+			Assert::IsTrue(scope.ToString() == string());
+
+			//Checking ToString() for int, string and Table/Scope
+			int32_t firstPushedInt = 10;
+			string firstPushedString = "Hello";
+			Scope *firstPushedScope = new Scope();
+
+			Datum& firstAppend = scope[nameOne];
+			firstAppend.SetType(DatumType::INT32_T);
+			firstAppend.PushBack(firstPushedInt);
+
+			Datum& secondAppend = scope[nameTwo];
+			secondAppend.SetType(DatumType::STRING);
+			secondAppend.PushBack(firstPushedString);
+
+			Datum& thirdAppend = scope[nameThree];
+			thirdAppend.SetType(DatumType::TABLE);
+			thirdAppend.PushBack(firstPushedScope);
+
+			string firstConvertedString = "INT32_T: 1, STRING: 1, TABLE: 1";
+			Assert::IsTrue(scope.ToString() == firstConvertedString);
+
+			//Checking ToString() for float, vec4, mat4x4
+			float_t firstPushedFloat = 10.0;
+
+			glm::vec4 firstPushedVec4(10, 20, 30, 40);
+			glm::vec4 secondPushedVec4(50, 60, 70, 80);
+			glm::vec4 thirdPushedVec4(90, 100, 110, 120);
+			glm::vec4 fourthVec4(90, 100, 110, 120);
+
+			glm::mat4x4 firstPushedMat4x4(firstPushedVec4, secondPushedVec4, thirdPushedVec4, fourthVec4);
+
+			string nameFour = "nameFour";
+			string nameFive = "nameFive";
+			string nameSix = "nameSix";
+
+			Datum& fourthAppend = scope[nameFour];
+			fourthAppend.SetType(DatumType::FLOAT);
+			fourthAppend.PushBack(firstPushedFloat);
+
+			Datum& fifthAppend = scope[nameFive];
+			fifthAppend.SetType(DatumType::GLM_VECTOR4);
+			fifthAppend.PushBack(firstPushedVec4);
+
+			Datum& sixthAppend = scope[nameSix];
+			sixthAppend.SetType(DatumType::GLM_MATRIX4X4);
+			sixthAppend.PushBack(firstPushedMat4x4);
+
+			string secondConvertedString = "INT32_T: 1, STRING: 1, TABLE: 1, FLOAT: 1, GLM_VECTOR4: 1, GLM_MATRIX4X4: 1";
+			Assert::IsTrue(scope.ToString() == secondConvertedString);
+		}
+
+		TEST_METHOD(ScopeTestAssignmentOperator)
+		{
+			Scope scope;
+			string nameOne = "Hello";
+			string nameTwo = "World";
+			string nameThree = "Now";
+
+			int32_t firstPushedInt = 10;
+			string firstPushedString = "Hello";
+
+			Datum& firstAppend = scope[nameOne];
+			firstAppend.SetType(DatumType::INT32_T);
+			firstAppend.PushBack(firstPushedInt);
+
+			Datum& secondAppend = scope[nameTwo];
+			secondAppend.SetType(DatumType::STRING);
+			secondAppend.PushBack(firstPushedString);
+
+			Scope& childScopeLevelOne = scope.AppendScope(nameThree);
+
+			//Inserting into first level nested scope
+			Datum& firstAppendLevelOne = childScopeLevelOne[nameOne];
+			firstAppendLevelOne.SetType(DatumType::INT32_T);
+			firstAppendLevelOne.PushBack(firstPushedInt);
+
+			Datum& secondAppendLevelOne = childScopeLevelOne[nameTwo];
+			secondAppendLevelOne.SetType(DatumType::STRING);
+			secondAppendLevelOne.PushBack(firstPushedString);
+
+			Scope& childScopeLevelTwo = childScopeLevelOne.AppendScope(nameThree);
+
+			//Inserting into second level nested scope
+			Datum& firstAppendLevelTwo = childScopeLevelTwo[nameOne];
+			firstAppendLevelTwo.SetType(DatumType::INT32_T);
+			firstAppendLevelTwo.PushBack(firstPushedInt);
+
+			Datum& secondAppendLevelTwo = childScopeLevelTwo[nameTwo];
+			secondAppendLevelTwo.SetType(DatumType::STRING);
+			secondAppendLevelTwo.PushBack(firstPushedString);
+
+			Datum& thirdAppendLevelTwo = childScopeLevelTwo.Append(nameThree);
+			thirdAppendLevelTwo.SetType(DatumType::TABLE);
+
+			Scope testScopeOne;
+			testScopeOne = scope;
+			Assert::IsTrue(testScopeOne == scope);
+			Assert::IsTrue(testScopeOne.GetParent() == nullptr);
+			secondAppendLevelTwo.Set("testFalse");
+			Assert::IsFalse(testScopeOne == scope);
+
+			Scope testScopeTwo;
+			testScopeTwo = childScopeLevelOne;
+			Assert::IsTrue(testScopeTwo == childScopeLevelOne);
+			Assert::IsTrue(testScopeTwo.GetParent() == nullptr);
+			secondAppendLevelOne.Set("testFalse");
+			Assert::IsFalse(testScopeTwo == childScopeLevelOne);
+
+			Scope testScopeThree;
+			testScopeThree = childScopeLevelTwo;
+			Assert::IsTrue(testScopeThree == childScopeLevelTwo);
+			Assert::IsTrue(testScopeThree.GetParent() == nullptr);
+			secondAppendLevelTwo.Set("something");
+			Assert::IsFalse(testScopeThree == childScopeLevelTwo);
+		}
+
+		TEST_METHOD(ScopeTestCopyConstructor)
+		{
+			Scope scope;
+			string nameOne = "Hello";
+			string nameTwo = "World";
+			string nameThree = "Now";
+
+			int32_t firstPushedInt = 10;
+			string firstPushedString = "Hello";
+
+			Datum& firstAppend = scope[nameOne];
+			firstAppend.SetType(DatumType::INT32_T);
+			firstAppend.PushBack(firstPushedInt);
+
+			Datum& secondAppend = scope[nameTwo];
+			secondAppend.SetType(DatumType::STRING);
+			secondAppend.PushBack(firstPushedString);
+
+			Scope& childScopeLevelOne = scope.AppendScope(nameThree);
+
+			//Inserting into first level nested scope
+			Datum& firstAppendLevelOne = childScopeLevelOne[nameOne];
+			firstAppendLevelOne.SetType(DatumType::INT32_T);
+			firstAppendLevelOne.PushBack(firstPushedInt);
+
+			Datum& secondAppendLevelOne = childScopeLevelOne[nameTwo];
+			secondAppendLevelOne.SetType(DatumType::STRING);
+			secondAppendLevelOne.PushBack(firstPushedString);
+
+			Scope& childScopeLevelTwo = childScopeLevelOne.AppendScope(nameThree);
+
+			//Inserting into second level nested scope
+			Datum& firstAppendLevelTwo = childScopeLevelTwo[nameOne];
+			firstAppendLevelTwo.SetType(DatumType::INT32_T);
+			firstAppendLevelTwo.PushBack(firstPushedInt);
+
+			Datum& secondAppendLevelTwo = childScopeLevelTwo[nameTwo];
+			secondAppendLevelTwo.SetType(DatumType::STRING);
+			secondAppendLevelTwo.PushBack(firstPushedString);
+
+			childScopeLevelTwo.AppendScope(nameThree);
+
+			Scope testScopeOne = scope;
+			Assert::IsTrue(testScopeOne == scope);
+			Assert::IsTrue(testScopeOne.GetParent() == nullptr);
+			secondAppendLevelTwo.Set("testFalse");
+			Assert::IsFalse(testScopeOne == scope);
+
+			Scope testScopeTwo = childScopeLevelOne;
+			Assert::IsTrue(testScopeTwo == childScopeLevelOne);
+			Assert::IsTrue(testScopeTwo.GetParent() == nullptr);
+			secondAppendLevelOne.Set("testFalse");
+			Assert::IsFalse(testScopeTwo == childScopeLevelOne);
+
+			Scope testScopeThree = childScopeLevelTwo;
+			Assert::IsTrue(testScopeThree == childScopeLevelTwo);
+			Assert::IsTrue(testScopeThree.GetParent() == nullptr);
+			secondAppendLevelTwo.Set("something");
+			Assert::IsFalse(testScopeThree == childScopeLevelTwo);
+		}
 	};
 }

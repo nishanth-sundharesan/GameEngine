678b5a187b7b30d8d205e788c0ebb0d027788126
Nishanth Sundharesan <nishanth.reddy992@gmail.com>
2017-02-26 13:11:22 -0500

    Added Glm::vec4 and Glm::mat4x4 to the pre compiled header.

--- Source/Library.Desktop.Test/Pch.h
+++ Source/Library.Desktop.Test/Pch.h
@@ -3,3 +3,9 @@
 // Standard libraries
 #include <vector>
 #include <cstdint>
+
+#pragma warning ( push )
+#pragma warning ( disable: 4201 )							//Suppressing the warning message "nonstandard extension used : nameless struct / union" in the file"
+#include <Glm/vec4.hpp>
+#include <Glm/mat4x4.hpp>
+#pragma warning ( pop )

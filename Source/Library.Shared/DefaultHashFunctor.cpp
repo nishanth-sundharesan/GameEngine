813b60a4ff03e275c015b09f59eb074121484c0c
Nishanth Sundharesan <nishanth.reddy992@gmail.com>
2017-03-05 00:26:39 -0500

    Added Hash functors for const types and some TODO comments.

--- Source/Library.Shared/DefaultHashFunctor.cpp
+++ Source/Library.Shared/DefaultHashFunctor.cpp
@@ -11,6 +11,19 @@ namespace GameEngineLibrary
 
 		while (*key != '\0')
 		{
+			//hash = hash * hashprime+key[i]
+			hash += 65 * static_cast<uint8_t>(*key);
+			++key;
+		}
+		return hash;
+	}
+
+	uint32_t DefaultHashFunctor<const char*>::operator()(const char* key) const
+	{
+		uint32_t hash = 0;
+
+		while (*key != '\0')
+		{
 			hash += 65 * static_cast<uint8_t>(*key);
 			++key;
 		}
@@ -18,6 +31,17 @@ namespace GameEngineLibrary
 	}
 
 	uint32_t DefaultHashFunctor<string>::operator()(const string key) const
+	{
+		uint32_t hash = 0;
+
+		for (uint32_t i = 0; i < key.length(); ++i)
+		{
+			hash += 65 * static_cast<uint8_t>(key[i]);
+		}
+		return hash;
+	}
+
+	uint32_t DefaultHashFunctor<const string>::operator()(const string key) const
 	{
 		uint32_t hash = 0;
 
--- Source/Library.Shared/DefaultHashFunctor.h
+++ Source/Library.Shared/DefaultHashFunctor.h
@@ -1,25 +1,35 @@
 #pragma once
 namespace GameEngineLibrary
 {
+	//const std::unit32_t prime=31;
+
+	//Default compare hash functor
+
+	// TODO
+	//bool DefaultCompare<TKey>::operator()(const TKey& lhs, const TKey& rhs) const
+
+	//{
+	//	return lhs == rhs;
+	//}
+
 	template<class TKey>
 	class DefaultHashFunctor
 	{
 	public:
 		//Perform a Bernstein's Hash
-		std::uint32_t DefaultHashFunctor::operator()(const TKey& key) const
-		{
-			uint32_t hash = 0;
-			uint8_t* byteArray = reinterpret_cast<uint8_t*>(const_cast<TKey*>(&key));
-			for (uint32_t i = 0; i < sizeof(key); ++i)
-			{
-				hash += 65 * byteArray[i];
-			}
-			return hash;
-		}
+		std::uint32_t DefaultHashFunctor::operator()(const TKey& key) const;
 	};
 
 	template<>
 	class DefaultHashFunctor<char*>
+	{
+	public:
+		//Perform a Bernstein's Hash
+		std::uint32_t DefaultHashFunctor::operator()(const char* key) const;
+	};
+
+	template<>
+	class DefaultHashFunctor<const char*>
 	{
 	public:
 		//Perform a Bernstein's Hash
@@ -33,4 +43,14 @@ namespace GameEngineLibrary
 		//Perform a Bernstein's Hash
 		std::uint32_t DefaultHashFunctor::operator()(const std::string key) const;
 	};
+
+	template<>
+	class DefaultHashFunctor<const std::string>
+	{
+	public:
+		//Perform a Bernstein's Hash
+		std::uint32_t DefaultHashFunctor::operator()(const std::string key) const;
+	};
 }
+
+#include "DefaultHashFunctor.inl"

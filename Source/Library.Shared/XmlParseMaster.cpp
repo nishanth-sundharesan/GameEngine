ddd06cf84947cb50b4489f84dad6f3f2850a10b9
Nishanth Sundharesan <nishanth.reddy992@gmail.com>
2017-03-16 22:59:08 -0400

    First take on function declarations.

--- Source/Library.Shared/XmlParseMaster.cpp
+++ Source/Library.Shared/XmlParseMaster.cpp
@@ -1,1 +1,113 @@
 #include "Pch.h"
+#include "XmlParseMaster.h"
+
+namespace GameEngineLibrary
+{
+	XmlParseMaster::SharedData::SharedData()
+	{
+	}
+
+	XmlParseMaster::SharedData* XmlParseMaster::SharedData::Clone()
+	{
+		return nullptr;
+	}
+
+	void XmlParseMaster::SharedData::SetXmlParseMaster()
+	{
+	}
+
+	XmlParseMaster* XmlParseMaster::SharedData::GetXmlParseMaster()
+	{
+		return nullptr;
+	}
+
+	void XmlParseMaster::SharedData::IncrementDepth()
+	{
+	}
+
+	void XmlParseMaster::SharedData::DecrementDepth()
+	{
+	}
+
+	int32_t XmlParseMaster::SharedData::Depth()
+	{
+		return depth;
+	}
+
+	XmlParseMaster::XmlParseMaster(SharedData* sharedData)
+	{
+		// TODO sharedData->   //Initialize sharedData
+		mParser = XML_ParserCreate(NULL);
+		// TODO Register static calls backs
+
+		mParser;
+		sharedData;
+	}
+
+	XmlParseMaster::~XmlParseMaster()
+	{
+
+	}
+
+	XmlParseMaster* XmlParseMaster::Clone()
+	{
+		return nullptr;
+	}
+
+	void XmlParseMaster::AddHelper(IXmlParseHelper* xmlParseHelper)
+	{
+		mXmlParseHelpers.PushBack(xmlParseHelper);
+	}
+
+	bool XmlParseMaster::RemoveHelper(IXmlParseHelper* xmlParseHelper)
+	{
+		return mXmlParseHelpers.Remove(xmlParseHelper);
+	}
+
+	void XmlParseMaster::Parse(char* rawXmlData, uint32_t length, bool isLastXmlChunkData)
+	{
+		XML_Parse(mParser, rawXmlData, length, isLastXmlChunkData);
+	}
+
+	void XmlParseMaster::ParseFromFile(char* fileName)
+	{
+		fileName;
+		XML_SetElementHandler(mParser, &XmlParseMaster::StartElementHandler, &XmlParseMaster::EndElementHandler);
+	}
+
+	char* XmlParseMaster::GetFileName()
+	{
+		return nullptr;
+	}
+
+	XmlParseMaster::SharedData* XmlParseMaster::GetSharedData()
+	{
+		return nullptr;
+	}
+
+	void XmlParseMaster::SetSharedData(SharedData* sharedData)
+	{
+		sharedData;
+	}
+
+	void XmlParseMaster::StartElementHandler(void *userData, const char *name, const char **atts)
+	{
+		userData;
+		name;
+		atts;
+		/*for (auto& value : mXmlParseHelpers)
+		{
+			
+		}*/				
+	}
+
+	void XmlParseMaster::EndElementHandler(void *userData, const char *name)
+	{
+		userData;
+		name;
+	}
+
+	void XmlParseMaster::CharDataHandler()
+	{
+	}
+}
--- Source/Library.Shared/XmlParseMaster.h
+++ Source/Library.Shared/XmlParseMaster.h
@@ -1,1 +1,69 @@
 #pragma once
+#include <expat.h>
+#include "IXmlParseHelper.h"
+#include "Vector.h"
+
+namespace GameEngineLibrary
+{
+	class XmlParseMaster
+	{
+	public:
+		class SharedData
+		{
+		public:
+			SharedData();
+
+			SharedData* Clone();
+
+			void SetXmlParseMaster();
+
+			XmlParseMaster* GetXmlParseMaster();
+
+			void IncrementDepth();
+
+			void DecrementDepth();
+
+			int32_t Depth();
+		private:
+			XmlParseMaster* xmlParseMaster;
+
+			int32_t depth;
+		};
+
+	public:
+		XmlParseMaster(SharedData* sharedData);
+
+		~XmlParseMaster();
+
+		XmlParseMaster* Clone();
+
+		void AddHelper(IXmlParseHelper* xmlParseHelper);
+
+		bool RemoveHelper(IXmlParseHelper* xmlParseHelper);
+
+		void Parse(char* rawXmlData, uint32_t length, bool isLastXmlChunkData);
+
+		void ParseFromFile(char* fileName);
+
+		char* GetFileName();
+
+		SharedData* GetSharedData();
+
+		void SetSharedData(SharedData* sharedData);
+	private:
+
+		static void StartElementHandler(void *userData, const char *name, const char **atts);
+
+		static void EndElementHandler(void *userData, const char *name);
+
+		static void CharDataHandler();
+	private:
+		XML_Parser mParser;
+
+		char* mCurrentParsingFile;
+
+		SharedData* mSharedData;
+
+		Vector<IXmlParseHelper*> mXmlParseHelpers;
+	};
+}

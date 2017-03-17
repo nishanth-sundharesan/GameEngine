1789cabda9009d8841d6e620479b072d906f2435
Nishanth Sundharesan <nishanth.reddy992@gmail.com>
2017-03-16 22:58:55 -0400

    First take on Function declarations.

--- Source/Library.Shared/IXmlParseHelper.h
+++ Source/Library.Shared/IXmlParseHelper.h
@@ -1,1 +1,19 @@
 #pragma once
+
+class IXmlParseHelper
+{
+public:
+	void Initialize();
+
+	// Can return bool
+	void StartElementHandler(void* userData, const char* name, const char* args[]);
+
+	// Can return bool
+	void EndElementHandler(void* userData, const char* name);
+
+	void CharDataHandler(void* userData, const char* value, int length);
+
+	virtual IXmlParseHelper* Clone() = 0;
+
+	virtual ~IXmlParseHelper();
+};

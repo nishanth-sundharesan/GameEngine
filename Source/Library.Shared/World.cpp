547a48bc1520224e9c6ed45a19254291fb576b73
Nishanth Sundharesan <nishanth.reddy992@gmail.com>
2017-03-31 14:49:12 -0400

    Implemented World.cpp and World.h

--- Source/Library.Shared/World.cpp
+++ Source/Library.Shared/World.cpp
@@ -1,37 +1,73 @@
 #include "Pch.h"
 #include "World.h"
+#include <assert.h>
 
 using namespace std;
 
 namespace GameEngineLibrary
 {
-	string World::Name()
+	RTTI_DEFINITIONS(World);
+
+	const string World::mSectorsName = "Sectors";
+
+	World::World(const string& name)
+		:mName(name), mSectorDatum(nullptr)
 	{
-		return string();
+		mName = name;
+	}
+
+	string& World::Name()
+	{
+		return const_cast<string&>(const_cast<const World*>(this)->Name());
+	}
+
+	const string& World::Name() const
+	{
+		return mName;
 	}
 
 	void World::SetName(const string& name)
 	{
-		name;
+		mName = name;
 	}
 
 	Datum& World::Sectors()
 	{
-		return tempDatum;
+		return const_cast<Datum&>(const_cast<const World*>(this)->Sectors());
 	}
 
 	const Datum& World::Sectors() const
+	{		
+		return *mSectorDatum;
+	}
+
+	void World::AdoptSector(Sector& sector)
 	{
-		return tempDatum;
+		AddNestedScopeAttribute(sector, mSectorsName);
 	}
 
 	Sector& World::CreateSector(const string& instanceName)
 	{
-		instanceName;
-		return tempSector;
+		Sector* sector = new Sector(instanceName);
+		AddNestedScopeAttribute(*sector, mSectorsName);		
+		return *sector;
 	}
 
-	void World::Update()
+	void World::Update(WorldState& worldState)
 	{
+		worldState.SetCurrentWorld(*this);
+
+		assert(mSectorDatum != nullptr);
+		for (uint32_t i = 0; i < mSectorDatum->Size(); ++i)
+		{
+			assert(static_cast<Sector*>(mSectorDatum->Get<Scope*>(i))->Is(Sector::TypeIdClass()));
+			(static_cast<Sector*>(mSectorDatum->Get<Scope*>(i)))->Update(worldState);
+		}
+	}
+
+	void World::InitializeSignatures()
+	{
+		AddExternalAttribute("WorldName", &mName, 1);
+		mSectorDatum = &AddEmptyNestedScopeAttribute(mSectorsName);
 	}
 }
--- Source/Library.Shared/World.h
+++ Source/Library.Shared/World.h
@@ -5,25 +5,38 @@ namespace GameEngineLibrary
 {
 	class World final :public Attributed
 	{
-	public:
-		//TODO Can be made const std::string&
-		std::string Name();
+	public:		
+		World(const std::string& name);
+
+		World(const World&) = delete;
+
+		World& operator=(const World&) = delete;
+
+		std::string& Name();
+
+		const std::string& Name() const;
 
 		void SetName(const std::string& name);
+
+		Sector& CreateSector(const std::string& instanceName);
 
 		Datum& Sectors();
 
 		const Datum& Sectors() const;
 
-		Sector& CreateSector(const std::string& instanceName);
+		void AdoptSector(Sector& sector);		
 
-		void Update();
+		void Update(WorldState& worldState);
 
 	private:
+		void InitializeSignatures();
+
 		std::string mName;
 
-		Datum tempDatum;
+		Datum* mSectorDatum;
 
-		Sector tempSector;
+		static const std::string mSectorsName;
+	public:
+		RTTI_DECLARATIONS(World, Attributed);
 	};
 }

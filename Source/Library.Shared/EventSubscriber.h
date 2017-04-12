36f0dd5e0989b393e2902e2300af20f1040bf6d1
Nishanth Sundharesan <nishanth.reddy992@gmail.com>
2017-04-12 17:32:59 -0400

    Initial commit of the base class EventSubscriber.h Added comments. Performed unit testing.

---
+++ Source/Library.Shared/EventSubscriber.h
@@ -0,0 +1,35 @@
+#pragma once
+
+namespace GameEngineLibrary
+{
+	/** Forward declared EventPublisher class.
+	*/
+	class EventPublisher;
+
+	/** Abstract base class for all event subscribers.
+	*/
+	class EventSubscriber
+	{
+	public:
+		/** Defaulted constructor.
+		*/
+		EventSubscriber() = default;
+
+		/** Defaulted copy constructor.
+		*/
+		EventSubscriber(const EventSubscriber&) = default;
+
+		/** Defaulted assignment operator.
+		*/
+		EventSubscriber& operator=(const EventSubscriber&) = default;
+
+		/** Pure virtual function Notify. Derived classes must override this function to get notifications.
+		*	@param eventPublisher The event publisher which contains the message.
+		*/
+		virtual void Notify(const EventPublisher& eventPublisher) = 0;
+
+		/** Defaulted virtual destructor.
+		*/
+		virtual ~EventSubscriber() = default;
+	};
+}

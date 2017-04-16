2772a0198cdecc219fa3fb97d0719078f4a9012d
Nishanth Sundharesan <nishanth.reddy992@gmail.com>
2017-04-16 12:06:19 -0400

    Added comments. ActionEvent now takes a SubType as datum and the delay as a string. Completed unit testing.

--- Source/Library.Shared/ActionEvent.cpp
+++ Source/Library.Shared/ActionEvent.cpp
@@ -15,20 +15,34 @@ namespace GameEngineLibrary
 	const string ActionEvent::sSubTypeAttributeName = "SubType";
 
 	ActionEvent::ActionEvent()
-		:mDelay(0), mSubType(0)
+		:mDelay(0), mSubType(string())
 	{
 		InitializeSignatures();
 	}
 
 	void ActionEvent::Update(WorldState& worldState)
 	{
-		EventMessage eventMessage(mSubType, *worldState.GetCurrentWorld(), worldState);
+		worldState.SetCurrentAction(this);
+
+		EventMessage eventMessage(mSubType, worldState);
 		eventMessage.CopyAuxiliaryAttributes(*this);
 
 		Event<EventMessage>* event = new Event<EventMessage>(eventMessage, true);
 		worldState.GetCurrentWorld()->GetEventQueue().Enqueue(*event, worldState.GetGameTime(), mDelay);
+
+		worldState.SetCurrentAction(nullptr);
 	}
 
+	void ActionEvent::SetSubTypeAndDelay(const Datum& datum, const string& delay)
+	{
+		if (datum.Type() != DatumType::STRING)
+		{
+			throw exception("Invalid DatumType for the SubType");
+		}
+		mSubType = datum.Get<string>();
+		sscanf_s(delay.c_str(), "%d", &mDelay);
+	}
+	
 	void ActionEvent::InitializeSignatures()
 	{
 		Action::InitializeSignatures();
--- Source/Library.Shared/ActionEvent.h
+++ Source/Library.Shared/ActionEvent.h
@@ -3,31 +3,60 @@
 
 namespace GameEngineLibrary
 {
+	/** ActionEvent class which generates events.
+	*/
 	class ActionEvent :public Action
 	{
-	public:		
+	public:
+		/** Default constructor. Defaults the private members of the class and initializes the signatures.
+		*/
 		ActionEvent();
 
+		/** Marked copy constructor as delete. Cannot copy ActionEvents.
+		*/
 		ActionEvent(const ActionEvent&) = delete;
 
+		/** Marked assignment operator as delete. Cannot copy ActionEvents.
+		*/
 		ActionEvent& operator=(const ActionEvent&) = delete;
 
+		/** Creates attributed EventMessages and Enqueues it with the set delay.
+		*	@param worldState The WorldState object of the game engine.
+		*/
 		virtual void Update(WorldState& worldState) override;
 
+		/** Sets the subType and the delay for the generated Events.
+		*	@param subType A Datum containing the subType for the Events to be generated.
+		*	@param delay The delay for the EventMessages which are generated in the Update().
+		*/
+		void SetSubTypeAndDelay(const Datum& subType, const std::string& delay);
+
+		/** Defaulted virtual destructor.
+		*/
 		virtual ~ActionEvent() = default;
 	protected:
 		/** Initializes the the signatures(member variables) of the class.
 		*/
 		virtual void InitializeSignatures();
 	private:
+		/** The name for the prescribed attribute delay.
+		*/
 		static const std::string sDelayAttributeName;
 
+		/** The name for the prescribed attribute subType.
+		*/
 		static const std::string sSubTypeAttributeName;
 
+		/** Cached delay. Events will be generated with this delay.
+		*/
 		std::int32_t mDelay;
 
-		std::int32_t mSubType;
+		/** Cached subType. Events will be generated with this subType.
+		*/
+		std::string mSubType;
 	public:
 		RTTI_DECLARATIONS(ActionEvent, Action);
 	};
+
+	ActionFactory(ActionEvent);
 }
